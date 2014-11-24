8cfe32eba7bbea73a8a94df18d513a23018bfa5d
Russell Gray <russgray@gmail.com>
2014-11-24 17:22:34 +0000

    Tests for getting events via legacy api

--- tests/legacy_tests.py
+++ tests/legacy_tests.py
@@ -23,3 +23,27 @@ def test_login():
     assert r.errorCode == 'OK'
     assert r.header.sessionToken == body['sessionToken']
 
+
+@responses.activate
+def test_get_active_event_types():
+    body = [{'eventType': {'id': 1, 'name': 'Soccer'}}]
+
+    # getActiveEventTypes should call through to listEventTypes
+    responses.add(responses.POST,
+        'https://api.betfair.com/exchange/betting/rest/v1.0/listEventTypes/',
+        body=json.dumps(body),
+        status=200,
+        content_type='application/json')
+
+    g = betfairng.BFGlobalService('')
+    r = g.getActiveEventTypes('')
+
+    assert len(r.eventTypeItems) == 1
+    assert r.eventTypeItems[0].name == 'Soccer'
+
+
+@raises(betfairng.LegacyOperationNotSupportedError)
+def test_get_events():
+    g = betfairng.BFGlobalService('')
+    g.getEvents('', 1)
+    assert False # shouldn't get here!
